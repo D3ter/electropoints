19a5ff397b90a1c669f71b4b414ec1ca249b8c30
tholowy <dast3rit@gmail.com>
2022-12-11 19:46:23 -0500

    init charge

---
+++ app/src/main/java/com/example/electroapp/ui/points/newPoint.java
@@ -0,0 +1,16 @@
+package com.example.electroapp.ui.points;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.electroapp.R;
+
+public class newPoint extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_point);
+    }
+}
