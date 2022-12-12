807555a47719e8450d83976215bbf0bfa308ef79
tholowy <dast3rit@gmail.com>
2022-12-11 21:45:00 -0500

    nueva vista

--- app/src/main/java/com/example/electroapp/ui/points/newPoint.java
+++ app/src/main/java/com/example/electroapp/ui/points/newPoint.java
@@ -7,10 +7,18 @@ import android.os.Bundle;
 import com.example.electroapp.R;
 
 public class newPoint extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_point);
+
+        this.setTitle("Crear punto");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return false;
     }
 }
--- app/src/main/java/com/example/electroapp/ui/points/points.java
+++ app/src/main/java/com/example/electroapp/ui/points/points.java
@@ -1,13 +1,17 @@
 package com.example.electroapp.ui.points;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.os.Debug;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.example.electroapp.MainActivity;
 import com.example.electroapp.R;
 
 /**
@@ -15,7 +19,7 @@ import com.example.electroapp.R;
  * Use the {@link points#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class points extends Fragment {
+public class points extends Fragment implements View.OnClickListener {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -25,7 +29,8 @@ public class points extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-
+    View view;
+    Button btn_add;
     public points() {
         // Required empty public constructor
     }
@@ -55,12 +60,26 @@ public class points extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_points, container, false);
+        view = inflater.inflate(R.layout.fragment_points, container, false);
+        btn_add = (Button) view.findViewById(R.id.btn_add);
+        btn_add.setOnClickListener(this);
+        return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        //do what you want to do when button is clicked
+        switch (v.getId()) {
+            case R.id.btn_add:
+                startActivity(new Intent(getActivity(), newPoint.class));
+                break;
+        }
     }
 }
